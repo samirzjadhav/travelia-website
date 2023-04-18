b1530a0e8e7384ef21ff119e86e2c9e4bd2208dc
Samir Jadhav <samirzjadhav@gmail.com>
2023-04-18 12:59:08 +0530

    feat: added script for navbar toggle

--- src/script.js
+++ src/script.js
@@ -3,3 +3,24 @@ const preLodaer = document.querySelector("[data-preloader]");
 window.addEventListener("load", () => {
   preLodaer.classList.add("remove");
 });
+
+// add Event on multiple element
+
+const addEventOnElement = function (element, eventType, callBack) {
+  for (let i = 0, len = element.length; i < len; i++) {
+    element[i].addEventListener(eventType, callBack);
+  }
+};
+
+// Navbar toggler for mobile
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelectorAll("[data-overlay]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElement(navTogglers, "click", toggleNav);
